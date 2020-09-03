7d1c2090f00d66970b0d8e9f9c9fb8bcf91c32ec
ramos-r <bckymoon@hotmail.com>
2020-09-02 21:22:38 -0300

    Adicionando a Build.

---
+++ precache-manifest.c7b250815d708f55ae0bbd8ab78d9793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecef5f630d42b36feca59e0eff09c50e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b782d44b6610798599db",
+    "url": "/static/css/main.35ec3be3.chunk.css"
+  },
+  {
+    "revision": "59c69e671c628011ea9e",
+    "url": "/static/js/2.74657459.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.74657459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b782d44b6610798599db",
+    "url": "/static/js/main.3d38e3e2.chunk.js"
+  },
+  {
+    "revision": "4b084925ca321bed2de4",
+    "url": "/static/js/runtime-main.9914da59.js"
+  }
+]);
